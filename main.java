8684c286aa267e530b4fd4241489abafc400a93b
Rand ahmad <Ranooda.ag2@gmail.com>
2024-11-18 11:57:20 +0300

    Modified

--- main.java
+++ main.java
@@ -1,133 +1,124 @@
 import java.util.Scanner;
 
 public class main {
-    public static Scanner input = new Scanner (System.in);
-    public static searchEngine SE = new searchEngine();
-
-
-
+    public static Scanner Read = new Scanner (System.in);
+    public static searchEngine SEQ = new searchEngine();
 
     public static int menu()
     {
-        System.out.println("1. Boolean Retrieval. ");
-        System.out.println("2. Ranked Retrieval.");
-        System.out.println("3. Indexed Documents.");
-        System.out.println("4. Indexed Tokens.");
-        System.out.println("5. Exist.");
+        System.out.println("-----Menu-----");
+        System.out.println("1: Boolean Retrieval. ");
+        System.out.println("2: Ranked Retrieval.");
+        System.out.println("3: Indexed Documents.");
+        System.out.println("4: Indexed Tokens.");
+        System.out.println("5: Exit.");
 
-        System.out.println("enter choice");
-        int choice = input.nextInt();
-        return choice;
+        System.out.println("Welcome, Enter your choice or 5 to Exit: ");
+        int c1 = Read.nextInt();
+        return c1;
     }
 
 
-    public static void Boolean_Retrieval_menu()
+    public static void BooleanRetrievalMenu()
     {
-        String [] Questions = { "market AND sports"
-                ,"weather AND warming"
-                ,"business AND world"
-                ,"weather OR warming"
-                ,"market OR sports"
-                ,"market OR sports AND warming"};
+        String [] BooleanQueries = { "market AND sports","weather AND warming","business AND world","weather OR warming","market OR sports","market OR sports AND warming"};
 
-        int choice1 ;
-        System.out.println("################### Boolean Retrieval ####################");
+        int c2 ;
+        System.out.println("###################Boolean Retrieval####################");
 
-        System.out.println("1. index");
-        System.out.println("2. inverted index");
-        System.out.println("3. inverted index using BST");
-        System.out.println("4. inverted index using AVL");
-        System.out.println("enter your choice");
-        choice1 = input.nextInt();
+        System.out.println("1: index");
+        System.out.println("2: inverted index");
+        System.out.println("3: inverted index with BST");
+        System.out.println("4: inverted index with AVL");
+        System.out.println("Enter your choice: ");
+        c2 = Read.nextInt();
 
-        System.out.print("Q#: ");
-        for ( int i = 0 ; i < Questions.length; i++)
+        System.out.print("#Q: ");
+        for ( int i = 0 ; i < BooleanQueries.length; i++)
         {
-            String str = Questions[i];
+            String str = BooleanQueries[i];
             // 1 inverted index List , 2 inverted index AVL
 
             System.out.println(str);
             System.out.print("Result doc IDs: ");
-            SE.Boolean_Retrieval(str, choice1 ).print();
+            SEQ.Boolean_Retrieval(str, c2 ).print();
             System.out.println("\n");
         }
     }
 
-    public static void Ranked_Retrieval_menu()
+    public static void RankedRetrievalmMenu()
     {
-        String [] Questions = { "market sports"
-                ,"weather warming"
-                ,"business world market"};
+        String [] RankedQueries = { "market sports","weather warming","business world market"};
 
-        System.out.println("######## Ranked Retrieval ######## ");
-        System.out.println("1. index");
-        System.out.println("2. inverted index");
-        System.out.println("3. inverted index using BST");
-        System.out.println("4. inverted index using AVL");
-        System.out.println("enter your choice");
-        int choice2 = input.nextInt();
+        System.out.println("########## Ranked Retrieval ##########");
+        System.out.println("1: index");
+        System.out.println("2: inverted index");
+        System.out.println("3: inverted index With BST");
+        System.out.println("4: inverted index With AVL");
+        System.out.println("Enter your choice: ");
+        int choice2 = Read.nextInt();
 
-        System.out.print("Q#: ");
-        for ( int i = 0 ; i < Questions.length; i++)
+        System.out.print("##Q: ");
+        for ( int i = 0 ; i < RankedQueries.length; i++)
         {
-            String str = Questions[i];
+            String str = RankedQueries[i];
 
             System.out.println("## Q: " + str);
-            SE.Ranked_Retrieval(str, choice2);
+            SEQ.Ranked_Retrieval(str, choice2);
             System.out.println("\n");
         }
     }
 
-    public static void Indexed_Documents_menu()
+    public static void IndexedDocumentsMenu()
     {
         System.out.println("######## Indexed Documents ######## ");
-        System.out.println("Indexed Documents " + SE.index.indexes.length);
+        System.out.println("Indexed Documents " + SEQ.index.indexes.length);
     }
 
-    public static void Indexed_Tokens_menu()
+    public static void IndexedTokensMenu()
     {
-        System.out.println("######## Indexed Tokens ######## ");
-        System.out.println("tokens " + SE.tokens);
+        System.out.println("######### Indexed Tokens #########");
+        System.out.println("tokens " + SEQ.tokens);
     }
 
     public static void main(String[] args) {
 
-        SE.LoadData();
+        SEQ.LoadData();
 
         // TODO code application logic here
-        int choice;
+        int Menu;
 
         do {
-            choice = menu();
-            switch (choice)
+            Menu = menu();
+            switch (Menu)
             {
                 //Boolean Retrieval: to enter a Boolean query that will return a set of unranked documents
                 case 1:
-                    Boolean_Retrieval_menu();
+                    BooleanRetrievalMenu();
                     break;
 
                 //Ranked Retrieval: to enter a query that will return a ranked list of documents with their scores
                 case 2:
-                    Ranked_Retrieval_menu();
+                    RankedRetrievalmMenu();
                     break;
 
                 //Indexed Documents: to show number of documents in the index
                 case 3:
-                    Indexed_Documents_menu();
+                    IndexedDocumentsMenu();
                     break;
 
                 //Indexed Tokens: to show number of vocabulary and tokens in the index
                 case 4:
-                    Indexed_Tokens_menu();
+                    IndexedTokensMenu();
                     break;
 
                 case 5:
                     break;
 
                 default:
-                    System.out.println("bad choice, try again!");
+                    System.out.println("try again, choose from 1-4 or 5 to Exit: ");
             }
-        } while (choice != 5);
+        } while (Menu != 5);
     }
 
 }
